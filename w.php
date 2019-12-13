8ac687510e62ae7abcee4a2e8555178b0a55df88
Exey-5 <58844709+Exey-5@users.noreply.github.com>
2019-12-13 13:23:41 +0300

    Update w.php

--- w.php
+++ w.php
@@ -163,34 +163,7 @@ $page = $_GET[ 'page' ] ?? 1;
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-<!-- Facebook сф6 Pixel Code -->
-<?php if ( isset( $_GET[ 'pixel' ] ) ): ?>
-    <script>
-        !function (f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function () {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '<?php echo $_GET[ 'pixel' ] ?>');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=<?php echo $_GET[ 'pixel' ] ?>&ev=PageView&noscript=1"
-        /></noscript>
-    <!-- End Facebook Pixel Code -->
+
 <?php endif ?>
 
 </body>
